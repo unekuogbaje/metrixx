867ab9815ae930ac69c715a7ce0df19d8eac55c3
Uneku Ogbaje <unekuogbaje@gmail.com>
2023-08-11 19:03:28 +0100

    Updated Routing

--- src/components/dashboard/dashboard.jsx
+++ src/components/dashboard/dashboard.jsx
@@ -1,12 +1,21 @@
-import PageLayout from '../page-layout/page-layout';
+import Navbar from '../navbar/navbar';
+import Sidebar from '../sidebar/sidebar';
+import CardContainer from '../card-container/card-container';
 
 const Dashboard = () => {
   return (
-    <div className='w-screen'>
-      <PageLayout>
-        
-      </PageLayout>
+    <div className='flex'>
+    <Sidebar/>
+    <div>
+      <Navbar 
+        pageTitle='Dashboard'>
+      </Navbar>
+      <div>
+        <CardContainer/>
+      </div>
     </div>
+    
+  </div>
   )
 };
 
---
+++ src/routes/page-layout/page-layout.jsx
@@ -0,0 +1,11 @@
+import Dashboard from "../../components/dashboard/dashboard";
+
+const PageLayout = () => {
+    return (
+      <div className='w-screen'>
+        <Dashboard/>
+      </div>
+    )
+};
+
+export default PageLayout;
