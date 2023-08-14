489ea127f19be37e6af8c5c5eef13f3025ab5c29
Uneku Ogbaje <unekuogbaje@gmail.com>
2023-08-14 22:24:51 +0100

    Link Conversations page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import Dashboard from './routes/dashboard/dashboard';
+import Conversations from './routes/conversations/conversations';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path='/' element={<Dashboard/>}>
-          
+        <Route path='conversations' element={<Conversations/>} />
         </Route>
       </Routes>
     </div>
